923797aead2358f71c99df44ea3d816c65187731
Boris Dev <borisdev@Boriss-MacBook-Air.local>
2023-05-14 11:30:58 -0700

    git clone customer oh my zsh shell plugins

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ apt-get update \
 # custom shell 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
+#
+#
 # use requirements.txt
 pip install openai
 pip install ipdb
