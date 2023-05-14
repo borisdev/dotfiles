3c989f9006bff5c7594e04052f0f73b38bf0d699
Boris Dev <borisdev@Boriss-MacBook-Air.local>
2023-05-14 11:48:25 -0700

    git clone customer oh my zsh shell plugins

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ apt-get update \
 && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends
     neovim \
     fzf
-
+#
 # custom shell 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
