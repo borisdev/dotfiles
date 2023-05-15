bccea16201dccbd54028c2a184352c546e63e23f
Boris Dev <borisdev@Boriss-MacBook-Air.local>
2023-05-14 18:25:35 -0700

    install.sh comments

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
+# use zsh instead of bash so that we can take advantage of oh-my-zsh CLI plugins
 sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
 
+# install my snowflake ubuntu libraries
 apt-get clean
 apt-get update \
 && apt-get upgrade -y \
 && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends
     neovim \
     fzf
-#
-# custom shell 
+
+
+# custom oh-my-zsh plugins for fast CLI work (I think)
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#
-#
-# use requirements.txt
+
+# one day replace this wit pip install requirements.txt
 pip install openai
 pip install ipdb
