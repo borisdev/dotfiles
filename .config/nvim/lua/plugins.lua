add28167146755dee7ff91198542e0d2c403b67e
Boris Dev <borisdev@Boriss-MacBook-Air.local>
2023-05-14 18:13:03 -0700

    auto install neovim package manager

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,21 @@
 vim.cmd([[ 
+
+"
+" Rationale: When the vim-plug package manager does not exist then it must be auto installed in new containers and laptops
+" Source: modified this https://github.com/junegunn/vim-plug/wiki/tips#automatic-installation
+" We want this --> $HOME/.local/share/nvim/site/autoload/plug.vim
+" stdpath('data') denotes $HOME/.local/share/nvim
+
+let data_dir = stdpath('data') . '/site' 
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+
+"
+" List of the vim plugins (requirements.txt)
+"
+
 call plug#begin(stdpath('data') . '/plugged')
 Plug 'ellisonleao/glow.nvim'
 Plug 'neovim/nvim-lspconfig'
