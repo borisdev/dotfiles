9c4740b125321161f8bddc8fa1f86d900725a7b1
Boris <boris.dev@gmail.com>
2024-04-08 16:15:17 -0700

    lua image plugin minimal config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -43,13 +43,6 @@ require("lazy").setup({
         },
     },
     {
-        "3rd/image.nvim",
-        dependencies = { "luarocks.nvim" },
-        config = function()
-            -- ...
-        end
-    },
-    {
     "benlubas/molten-nvim",
     version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
     build = ":UpdateRemotePlugins",
---
+++ .config/nvim/lua/test_markup.md
@@ -0,0 +1,4 @@
+# Hello World
+
+![This is a remote image](https://gist.ro/s/remote.png)
+]]
