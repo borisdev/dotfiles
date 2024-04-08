9d0e1b614cd76c917c6d02d7813e9ac3e01ff99f
Boris <boris.dev@gmail.com>
2024-04-08 16:32:40 -0700

    lua image plugin minimal config

--- .config/nvim/minimal-setup.lua
+++ .config/nvim/minimal-setup.lua
@@ -57,3 +57,21 @@ require("lazy").setup({
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
 package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
 
+vim.opt.number = true
+vim.opt.conceallevel = 2
+vim.opt.winbar = "image.nvim demo"
+vim.opt.signcolumn = "yes:2"
+
+local content = [[
+# Hello World
+
+![This is a remote image](https://gist.ro/s/remote.png)
+]]
+
+vim.schedule(function()
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, true, vim.split(content, "\n"))
+  vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+  vim.api.nvim_set_current_buf(buf)
+  vim.cmd("split")
+end)
